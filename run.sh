8db3c0760dabec48b96f56a2f33dba7dc61df302
Arbaz <165058752+arbazahmed07@users.noreply.github.com>
2026-05-13 23:31:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ ! -t 0 ]; then
+    tmpfile=$(mktemp /tmp/hadoop_XXXX.sh)
+    cp /proc/$$/fd/255 "$tmpfile" 2>/dev/null
+    chmod +x "$tmpfile"
+    exec bash "$tmpfile" < /dev/tty
+fi
+
 echo "🔹 Starting Hadoop services..."
 start-dfs.sh
 start-yarn.sh
@@ -18,12 +25,10 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
-
 public class WordCount {
     public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
-
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             StringTokenizer itr = new StringTokenizer(value.toString());
             while (itr.hasMoreTokens()) {
@@ -32,10 +37,8 @@ public class WordCount {
             }
         }
     }
-
     public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
         private IntWritable result = new IntWritable();
-
         public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) sum += val.get();
@@ -43,22 +46,17 @@ public class WordCount {
             context.write(key, result);
         }
     }
-
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
-
         job.setJarByClass(WordCount.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
-
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
@@ -66,11 +64,8 @@ EOF
 
 echo "🔹 Creating input file..."
 echo "👉 Enter text (multi-line). Type 'END' on a new line to finish:"
-
 > input.txt
-
-while true; do
-    read line
+while IFS= read -r line < /dev/tty; do
     if [ "$line" = "END" ]; then
         break
     fi
@@ -79,8 +74,7 @@ done
 
 echo "🔹 Compiling..."
 mkdir -p classes
-javac -classpath `hadoop classpath` -d classes WordCount.java
-
+javac -classpath $(hadoop classpath) -d classes WordCount.java
 if [ $? -ne 0 ]; then
     echo "❌ Compilation failed"
     exit 1
@@ -99,7 +93,6 @@ hdfs dfs -put input.txt /input
 
 echo "🔹 Running MapReduce job..."
 hadoop jar wordcount.jar WordCount /input /output
-
 if [ $? -ne 0 ]; then
     echo "❌ Job failed"
     exit 1
